3ee5a4a3feb965d2d0032e36759fc5f9b724cf83
alakimov1 <alakimov1@yandex.ru>
2023-05-05 02:25:46 +0300

    замощение

--- src/app/visualization/visualization.component.ts
+++ src/app/visualization/visualization.component.ts
@@ -1,5 +1,19 @@
 import { Component } from '@angular/core';
-     
+    
+export class LaminateCell{
+    constructor(
+        public color:string,
+        public width:number   
+    ){}
+}
+
+export class LaminateRow{
+    constructor(
+        public height:number,
+        public laminates:LaminateCell[]
+    ){}
+}
+
 @Component({
     selector: 'visualization-comp',
     templateUrl: './visualization.component.html',
@@ -9,25 +23,25 @@ import { Component } from '@angular/core';
 export class VisualizationComponent { 
 
     colors:string[];
-
+    scale:number;
     room:{
         width:number,
         height:number,
     };
 
-    laminatesRows:{
-        height:number,
-        laminates:{
-            color:string,
-            width:number
-        }
-    }[];
+    laminateRows:LaminateRow[]=[];
 
-    currentColor:number;
+    currentColor:number=0;
 
     startVisualization(settings)
     {
         this.colors=settings.laminateColors;
+
+        if (!this.colors)
+        {
+            return;
+        }
+
         if (!(this.currentColor>=0))
         {
             this.currentColor=0;
@@ -36,7 +50,51 @@ export class VisualizationComponent {
             width:settings.roomWidth,
             height:settings.roomLength
         }
-        console.log(this.room);
+
+        this.scale=this.room.width/this.room.height;
+        let height = this.room.height;
+        this.laminateRows=[];
+        let needShift = false;
+
+        while (height>0)
+        {
+            let rowHeight = height>settings.laminateLength
+                ?settings.laminateLength
+                :height;
+            height -=rowHeight;
+            
+            let width = this.room.width;
+            
+            let laminatesRow:LaminateCell[]=[];
+            let firstLaminate = true;
+
+            while(width>0)
+            {
+                let laminateWidth = width>settings.laminateWidth
+                    ?settings.laminateWidth
+                    :width;
+
+                if(firstLaminate
+                    && needShift)
+                    {
+                        laminateWidth /=2;
+                        firstLaminate = false;
+                    }
+
+                width -= laminateWidth;
+                laminatesRow.push({
+                    color:this.colors[this.currentColor].slice(),
+                    width:laminateWidth*100/this.room.width
+                });
+            }
+
+            needShift = !needShift;
+
+            this.laminateRows.push({
+                height:rowHeight*100/this.room.height,
+                laminates:laminatesRow
+            });
+        }
     }
 
     colorChanged(color)
